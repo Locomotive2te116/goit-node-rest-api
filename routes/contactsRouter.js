bfa6035dbce8f662954ef588a82cd18433c02b39
Locomotive2te116 <locomotive2te116@gmail.com>
2024-02-23 14:03:10 +0200

    4

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,23 +1,39 @@
 import express from "express";
 import contactsControllers from "../controllers/contactsControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import {
+  addSchema,
+  updateSchema,
+  updateFavoriteSchema,
+} from "../schemas/contactsSchemas.js";
+import isValidId from "../middlewares/isValidId.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAllContacts);
 
-contactsRouter.get("/:id", contactsControllers.getOneContact);
+contactsRouter.get("/:id", isValidId, contactsControllers.getOneContact);
 
 contactsRouter.delete("/:id", contactsControllers.deleteContact);
 
-contactsRouter.post("/", contactsControllers.createContact);
+contactsRouter.post(
+  "/",
+  validateBody(addSchema),
+  contactsControllers.createContact
+);
 
-contactsRouter.put("/:id", contactsControllers.updateContact);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateSchema),
+  contactsControllers.updateContact
+);
 
-// contactsRouter.patch(
-//     "/:id/favorite",
-//     isValidId,
-//     validateBody(updateFavoriteSchema),
-//     contactsController.updateStatusContact
-//   );
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(updateFavoriteSchema),
+  contactsControllers.updateStatusContact
+);
 
 export default contactsRouter;
