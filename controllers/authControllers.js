a4effce0e9ec993e1d1a95f7b26a022cda4c4847
Locomotive2te116 <locomotive2te116@gmail.com>
2024-03-04 19:31:31 +0200

    12

---
+++ controllers/authControllers.js
@@ -0,0 +1,50 @@
+import * as authServices from "../services/authServices.js";
+import * as userServices from "../services/userServices.js";
+import HttpError from "../helpers/HttpError.js";
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
+
+const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await userServices.findUser({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+
+  const newUser = await authServices.signup(req.body);
+  res.status(201).json({
+    email: newUser.email,
+    password: newUser.password,
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await userServices.findUser({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await authServices.setToken(user._id, token);
+
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin),
+};
